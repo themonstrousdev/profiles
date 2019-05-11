873760f305cdcf5b9b490c052132ee7295414756
Monster Dev <ellexidecodes@gmail.com>
2019-05-11 15:43:02 +0800

    does this work?

--- Addons/free.js
+++ Addons/free.js
@@ -412,7 +412,14 @@ $(window).ready(()=>{
         style: "display: flex; flex-flow: row wrap; justify-content: space-between;align-items: center;align-content: center;",
         html: "<span>Copy<span> <span style='opacity: .6;font-size: 80%'>Ctrl + C</span>",
         click: function() {
+          $("<textarea", {
+            id: "copyMe",
+            style: "opacity: 0;pointer-events: none",
+            value: hasSelected,
+          }).appendTo("body");
+          $("#copyMe").select();
           document.execCommand("copy");
+          $("#copyMe").remove();
         }
       }).appendTo(".context-menu");
     }
