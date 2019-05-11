79995759423d98dc56f19397abd69ebe25d003a8
Monster Dev <ellexidecodes@gmail.com>
2019-05-11 15:14:53 +0800

    change color for dark dev tools

--- Addons/commission.js
+++ Addons/commission.js
@@ -27,9 +27,9 @@ $(window).ready(()=>{
   
   console.log('%cSTOP!', 'color: red; text-shadow: 2px 0px #000, 0px -2px #000, -2px 0px #000, 0px 2px #000;font-size: 7vh; font-weight: 600;font-family:Calibri');
   
-  console.log('%cThis code is either paid for, copyrighted, or both! In an event that you find this, you are already trying to bypass that. Please turn back now and simply ask The Monster (aka Elle) for any question regarding this profile.', 'color: #333; font-size: 3vh;font-family: Calibri;');
+  console.log('%cThis code is either paid for, copyrighted, or both! In an event that you find this, you are already trying to bypass that. Please turn back now and simply ask The Monster (aka Elle) for any question regarding this profile.', 'color: #666; font-size: 3vh;font-family: Calibri;');
   
-  console.log('%cIf you want to know more, go to http://www.themonster.xyz', 'color: #333; font-size: 3vh;font-family: Calibri;');
+  console.log('%cIf you want to know more, go to http://www.themonster.xyz', 'color: #555; font-size: 3vh;font-family: Calibri;');
  
 
   $("head").append(`
