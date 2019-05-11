29b623fc33a18b54a3006741aaa1794ef8d10f7b
Monster Dev <ellexidecodes@gmail.com>
2019-05-11 11:31:20 +0800

    update

--- Commercialized/js/kama.js
+++ Commercialized/js/kama.js
@@ -2,5 +2,5 @@ var delay=0,images=$(".sprite .img").length,count,duration=3*images;$(".img").ea
 $(this).css("background-image",`url(${img})`);});$(".btn").each(function(){var img=$(this).attr("img");$("<div>",{class:"lotusIcon"}).appendTo($(this));$("<div>",{class:"manifest",style:`background:url(${img})right/auto 100%no-repeat`}).appendTo($(this));})
 $(".friend").each(function(){var img=$(this).attr('face');$(this).addClass("tooltip");if(!img){img="https://i.imgur.com/fzBjzh5.png";}
 $(this).css("background-image",`url(${img})`);})
-$(".sprite .btn").click(function(){var opens=$(`#${$(this).attr("opens")}`);if(opens.is(":visible")){return;}else if(!opens){$(".tab").fadeOut(500);}
+$(".sprite .btn").click(function(){var background=document.querySelector("video.background");background.play();var opens=$(`#${$(this).attr("opens")}`);if(opens.is(":visible")){return;}else if(!opens){$(".tab").fadeOut(500);}
 $(".tab").fadeOut(500);opens.show();$(".img").fadeOut(1000);$(`#${$(this).attr("portrait")}`).fadeIn(1000);});
