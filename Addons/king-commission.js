0df8e1e15cd9e9776ff33412a80c2f9d01cef2a4
Monster Dev <40906882+themonstrousdev@users.noreply.github.com>
2021-06-13 22:15:58 +0800

    Update king-commission.js

--- Addons/king-commission.js
+++ Addons/king-commission.js
@@ -111,7 +111,7 @@ $(window).ready(()=>{
 $(document).keydown(function (event) {
   if (event.keyCode == 123) {
     return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73 || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73 || (event.ctrlKey && event.shiftKey && event.keyCode == 74) || ((event.ctrlKey && event.keyCode == 85))) {
     return false;
   }
 });
